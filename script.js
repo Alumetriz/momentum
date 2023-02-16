6b98af937837d0d7ea3698bebe1cda728e865c54
Alumetriz <fanmax0808@gmail.com>
2023-02-16 16:19:27 +0200

    feat: implemented the Image Slider functionality

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const time = document.querySelector('.time')
 const date = document.querySelector('.date')
 const greeting = document.querySelector('.greeting')
 const name = document.querySelector('.name')
-greeting.textContent = '123123'
+const body = document.querySelector('body')
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
 
 /* Time and Date block */
 function showTime() {
@@ -13,6 +15,7 @@ function showTime() {
 	showGreeting()
 	setTimeout(showTime, 1000)
 }
+
 showTime();
 
 function showDate() {
@@ -27,8 +30,9 @@ function showDate() {
 
 function getHours() {
 	let time = new Date();
-	return  time.getHours();
+	return time.getHours();
 }
+
 function getTimeOfDay() {
 	const time = getHours();
 	let timeOfDay;
@@ -43,25 +47,66 @@ function getTimeOfDay() {
 	}
 	return timeOfDay
 }
+
 function showGreeting() {
 	const timeOfDay = getTimeOfDay();
 	greeting.textContent = `Good ${timeOfDay}`;
 }
 
-/* Шf reload - username is saved */
+/* If reload - username is saved */
+
 // Save Data
 function setLocalStorage() {
 	localStorage.setItem('name', name.value);
 }
+
 window.addEventListener('beforeunload', setLocalStorage)
 
 // Data Recovery and Display
 function getLocalStorage() {
-	if(localStorage.getItem('name')) {
+	if (localStorage.getItem('name')) {
 		name.value = localStorage.getItem('name');
 	}
 }
+
 window.addEventListener('load', getLocalStorage)
 /* if reload - username is saved */
 
 /* Time, Date and Greeting block */
+
+
+/* Image Slider */
+let randomNum;
+
+function getRandomNum() {
+	randomNum = Math.floor(Math.random() * 20) + 1
+	return randomNum
+}
+
+getRandomNum()
+
+function setBg() {
+	let timeOfDay = getTimeOfDay();
+	let bgNum = randomNum;
+	bgNum = bgNum.toString().padStart(2, '0')
+	const img = new Image();
+	img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`
+	img.onload = () => {
+		body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`
+	};
+}
+setBg()
+
+function getSlideNext() {
+	randomNum < 20 ? randomNum++ : randomNum = 1
+	setBg();
+}
+
+function getSlidePrev() {
+	randomNum > 1 ? randomNum-- : randomNum = 20
+	setBg()
+}
+
+slideNext.addEventListener('click', getSlideNext);
+slidePrev.addEventListener('click', getSlidePrev);
+/* Image Slider */
